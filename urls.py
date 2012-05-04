eb6e1b4b9778a99fc89a19f880161505cbae77f6
Olli Jarva <olli.jarva@futurice.com>
2012-05-04 14:54:07 +0300

    Don't fail too miserably with wrong URLs

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
+from django.core.urlresolvers import reverse_lazy
+from django.utils.functional import lazy
 
 urlpatterns = patterns('',
 	url(r'^$', 'files.views.main', name="index", kwargs={"template_name": "files/index.html"}),
+        url(r'^browse$', redirect_to, {"url": reverse_lazy("index") }),
+        url(r'^browse/$', redirect_to, {"url": reverse_lazy("index") }),
 	url(r'^browse/([a-z_]+)/(.*)$', 'files.views.browse', name="browse", kwargs={"template_name": "files/browse.html"}),
 	url(r'^upload/([a-z_]+)/(.*)$', 'files.views.upload', name="upload"),
 	url(r'^delete/([a-z_]+)/(.+)$', 'files.views.delete', name="delete"),
